7ceca325fb186717991fecd26d8e437f9efdd936
Steven Swenson <ctigeek@gmail.com>
2013-12-28 17:23:49 -0500

    Fixed a few bugs. Better app.js file. Examples in README.md.

--- README.md
+++ README.md
@@ -1,7 +1,38 @@
 NodeXmlDeserializer
 ===================
+**NOTE**: The NPM package for this project is called [XmlDeserializer](https://npmjs.org/package/xmldeserializer).
 
-XML de-serializer package for Node.
+Description: A simple and somewhat crude XML de-serializer package for Node.
 
-The JSON functionality doens't "look" at the xml namespaces, so be prepared for potentially overlapping names.
+*shut up and show me the code!*
+```javascript
+	var xml = "<root>";
+    xml += "<childelement someProp=\"somevalue\">";
+    xml += "contents of child element";
+    xml += "</childelement>";
+    xml += "</root>";
+
+    var ds = require("../lib/xmldeserializer");
+    var elements = ds.deserialize(xml);
+    var jsonString = ds.getJson(elements);
+    console.log(jsonString);
+```
+Output:
+```
+{"root":{"childelement":{"someProp":"somevalue","value":"contents of child element"}}}
+```
+
+[More examples in the app.js test file.](https://github.com/ctigeek/NodeXmlDeserializer/blob/master/test/app.js)
+
+Features:
+* No schema or object definitions! Just send it XML and out comes JSON!
+* Supports parsing of namespaces (but not JSON interpretation.)
+* Supports CDATA, including special characters.
+
+Limitations:
+* No support for streams. You must pass in the entire XML block in a string.
+* The getJson method currently doesn't take namespaces into account. If your xml schema has duplicate names in different namespaces this may be an issue for you.
+* The names of array elements must be included when instantiating the deserializer. This is the only information it needs prior to starting the process because it can't determine which elements will repeat during the parsing process. See app.js for an example.
+
+
 
--- test/app.js
+++ test/app.js
@@ -1,34 +1,109 @@
 ﻿
-var xml = "<root asdf=\"wer\"><firstObject prop=\"blah\"/><someObject prop=\"someproperty\">some value</someObject></root> ";
-var json = {root: { asdf:"wer", firstObject: { prop:"blah" }, someObject: { prop:"someproperty", value:"some value" }}};
+var basicTest = function() {
+    var xml = "<root>";
+    xml += "<childelement someProp=\"somevalue\">";
+    xml += "contents of child element";
+    xml += "</childelement>";
+    xml += "</root>";
 
-var xml2 = "<root><arrayOfStuff><item>qwe</item><item>zxv</item><item>tttt</item><specialItem>poiuyt</specialItem></arrayOfStuff></root>";
-var json2 = {root: { arrayOfStuff: { arrayOf_item:["qwe","zxv","tttt"], specialItem:"poiuyt" } } };
+    var ds = require("../lib/xmldeserializer");
+    var elements = ds.deserialize(xml);
+    var jsonString = ds.getJson(elements);
+    console.log(jsonString);
+    var jsonObject = JSON.parse(jsonString);
+    console.log(JSON.stringify( jsonObject, null, 4));
+    console.log("done!");
+};
 
-var xmlDefaultNS = "<root xmlns=\"http://blahblah.com/somenamespace\"><element someprop=\"somevalue\"/></root>";
+var arrayTest = function() {
+    var xml = "<root>";
+    xml += "<listOfItems itemCategory=\"blue\">";
+    xml += "<item>lagoon</item>";
+    xml += "<item>man group</item>";
+    xml += "<item>christmas</item>";
+    xml += "<item>light special</item>";
+    xml += "</listOfItems>";
+    xml += "</root>";
 
-var xmlMultiNS = "<ns1:root xmlns:ns1=\"http://blahblah.com/ns1\"><ns1:element someprop=\"somevalue\"/><ns2:element xmlns:ns2=\"http://blahblah.com/ns2\">element body</ns2:element></ns1:root>";
+    var ds = require("../lib/xmldeserializer");
+    var arrayOfArrayNames = new Array("item");
+    var elements = ds.deserialize(xml);
+    var jsonString = ds.getJson(elements,arrayOfArrayNames);
+    console.log(jsonString);
+    var jsonObject = JSON.parse(jsonString);
+    console.log(JSON.stringify( jsonObject, null, 4));
+    console.log("done!");
+};
 
-//var xmlCdataExample = "<root><element><![CDATA[ This is data!\r\n data data data!  ]]></element></root>";
+var cDataTest = function() {
+    var xml = "<root>";
+    xml += "<element>";
+    xml += "<![CDATA[ This is data!\r\n data data data! You can put special characters in here!\r\n\r\n Here's a quote: \" and here's a backslash: \\  ]]>";
+    xml += "</element>";
+    xml += "</root>";
 
-//var elements = extractElements(xml2);
-//var json = elements[0].getJson(new Array("item"));
-///console.log(json);
+    var ds = require("../lib/xmldeserializer");
+    var elements = ds.deserialize(xml);
+    var jsonString = ds.getJson(elements);
+    console.log(jsonString);
+    var jsonObject = JSON.parse(jsonString);
+    console.log(JSON.stringify( jsonObject, null, 4));
+    console.log("done!");
+};
 
-var ds = require("../lib/XmlDeserializer");
-var deserializer = new ds.deserializer(null);
-var elements = deserializer.deserialize(xmlMultiNS);
-var json = deserializer.getJson( elements);
+var defaultNamespaceTest = function() {
+    var xml = "<root xmlns=\"http://blahblah.com/somenamespace\">";
+    xml += "<element>";
+    xml += "<![CDATA[ This is data!\r\n data data data! You can put special characters in here!\r\n\r\n Here's a quote: \" and a backslash: \\  ]]>";
+    xml += "</element>";
+    xml += "</root>";
 
-//var str = "{\"root\":{\"arrayOfStuff\":{\"specialItem\":{\"value\":\"poiuyt\"},\"arrayOf_item\":[\"qwe\",\"zxv\",\"tttt\"]}}}";
+    var ds = require("../lib/xmldeserializer");
+    var elements = ds.deserialize(xml);
+    var jsonString = ds.getJson(elements);
+    console.log(jsonString);
+    var jsonObject = JSON.parse(jsonString);
+    console.log(JSON.stringify( jsonObject, null, 4));
+    console.log("done!");
+};
 
-//var str = "{\"root\":{\"arrayOfStuff\":\"blah\", \"morestuff\":\"asdf\"}}";
-//str = "{\"root\":5}";
+var namedNamespaceTest = function() {
+    var xml = "<rt:root xmlns:rt=\"http://blahblah.com/somenamespace\">";
+    xml += "<rt:element>";
+    xml += "yadda yadda yadda";
+    xml += "</rt:element>";
+    xml += "</rt:root>";
 
-//var str = "{\"root\":{\"element\":{\"value\":\" This is data! \\r\\n \\\" data data data!  \"}}}"
+    var ds = require("../lib/xmldeserializer");
+    var elements = ds.deserialize(xml);
+    var jsonString = ds.getJson(elements);
+    console.log(jsonString);
+    var jsonObject = JSON.parse(jsonString);
+    console.log(JSON.stringify( jsonObject, null, 4));
+    console.log("done!");
+};
 
-//var str = "{\"root\":{\"element\":{\"value\":\" \" asdf  !!!! \"}}}"
+var multipleNamespaceTest = function() {
+    var xml = "<rt:root xmlns:rt=\"http://blahblah.com/somenamespace\">";
+    xml += "<rt2:element xmlns:rt2=\"http://blahblah.com/anothernamespace\">";
+    xml += "yadda yadda yadda";
+    xml += "</rt2:element>";
+    xml += "</rt:root>";
 
-var jsonObject = JSON.parse(json);
-console.log(JSON.stringify( jsonObject, null, 4));
-console.log("done!");
+    var ds = require("../lib/xmldeserializer");
+    var elements = ds.deserialize(xml);
+    var jsonString = ds.getJson(elements);
+    console.log(jsonString);
+    var jsonObject = JSON.parse(jsonString);
+    console.log(JSON.stringify( jsonObject, null, 4));
+    console.log("done!");
+};
+
+
+basicTest();
+arrayTest();
+cDataTest();
+defaultNamespaceTest();
+namedNamespaceTest();
+multipleNamespaceTest();
+return;
